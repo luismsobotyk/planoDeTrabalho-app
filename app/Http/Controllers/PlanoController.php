f7a5abf8c22280fe81b020ca7716825afe44d088
Luis Sobotyk <luismsobotyk@gmail.com>
2024-11-20 14:45:40 -0300

    fix: removing unused lines

--- app/Http/Controllers/PlanoController.php
+++ app/Http/Controllers/PlanoController.php
@@ -10,6 +10,7 @@ use App\Models\Aula;
 use App\Models\InfoPessoais;
 use App\Models\Periodo;
 use App\Models\Plano;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class PlanoController extends Controller
@@ -52,81 +53,66 @@ class PlanoController extends Controller
         $ensDescricao = $request->input('ensinoDescricao');
         $ensCargaHoraria = $request->input('ensinoCargaHoraria');
 
-        //deletar todas essas multiplas e resalvar sempre que atualiza pra nao precisar ficar procurando atualizacao
+        InfoPessoais::updateOrCreate(
+            ['plano_id' => $request->input('plano_id')],
+            [
+                'categoria' => $request->input('categoria'),
+                'regime' => $request->input('regime')
+            ]
+        );
 
-        $infoPessoais = InfoPessoais::create([
-            'categoria' => $request->input('categoria'),
-            'regime' => $request->input('regime'),
-            'plano_id' => $request->input('plano_id')
-        ]);
-
-        $aulas = [];
         if(isset($aulasDisciplinas) && count($aulasDisciplinas)>0){
             for($i = 0; $i < count($aulasDisciplinas); $i++){
-                $aulas[] = Aula::create([
+                Aula::create([
                     'disciplina' => $aulasDisciplinas[$i],
                     'curso' => $aulasCursos[$i],
                     'carga_horaria' => $aulasCargasHorarias[$i],
                     'plano_id' => $request->input('plano_id')
                 ]);
             }
-        }else{
-            echo 'NENHUMA AULA INFORMADA';
         }
 
-        $atividadesAdministrativas = [];
         if(isset($admDescricao) && count($admDescricao)>0){
             for($i = 0; $i < count($admDescricao); $i++){
-                $atividadesAdministrativas[]= AtivAdministrativa::create([
+                AtivAdministrativa::create([
                     'descricao' => $admDescricao[$i],
                     'portaria' => $admPortaria[$i],
                     'carga_horaria' => $admCargaHoraria[$i],
                     'plano_id' => $request->input('plano_id')
                 ]);
             }
-        }else{
-            echo 'NENHUMA ATIVIDADE ADMINISTRATIVA INFORMADA';
         }
 
-        $atividadesExtensao = [];
         if(isset($extDescricao) && count($extDescricao)>0){
             for($i = 0; $i < count($extDescricao); $i++){
-                $atividadesExtensao[]= AtivExtensao::create([
+                AtivExtensao::create([
                     'descricao' => $extDescricao[$i],
                     'carga_horaria' => $extCargaHoraria[$i],
                     'plano_id' => $request->input('plano_id')
                 ]);
             }
-        }else{
-            echo 'NENHUMA ATIVIDADE DE EXTENSAO INFORMADA';
         }
 
-        $atividadesPesquisa = [];
         if(isset($pesqDescricao) && count($pesqDescricao)>0){
             for($i = 0; $i < count($pesqDescricao); $i++){
-                $atividadesPesquisa[]= AtivPesquisa::create([
+                AtivPesquisa::create([
                     'descricao' => $pesqDescricao[$i],
                     'carga_horaria' => $pesqCargaHoraria[$i],
                     'plano_id' => $request->input('plano_id')
                 ]);
             }
-        }else{
-            echo 'NENHUMA ATIVIDADE DE PESQUISA INFORMADA';
         }
 
-        $atividadesEnsino = [];
         if(isset($ensDescricao) && count($ensDescricao)>0){
             for($i = 0; $i < count($ensDescricao); $i++){
-                $atividadesEnsino[]= AtivEnsino::create([
+                AtivEnsino::create([
                     'descricao' => $ensDescricao[$i],
                     'carga_horaria' => $ensCargaHoraria[$i],
                     'plano_id' => $request->input('plano_id')
                 ]);
             }
-        }else{
-            echo 'NENHUMA ATIVIDADE DE ENSINO INFORMADA';
         }
 
-        return redirect()->back()->with('success', 'Plano salvo com Sucesso!'); //->withInput($request->input())
+        return redirect()->back()->with('success', 'Plano salvo com Sucesso!');
     }
 }
